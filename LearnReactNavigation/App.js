1202e35bcbec4d16e7d9ac89c25c0e62d8b67e3f
Taejung Kim <hi2177@naver.com>
2022-07-28 23:00:20 +0900

    chap5-2: 헤더 커스터마이징

--- LearnReactNavigation/App.js
+++ LearnReactNavigation/App.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+import {Text, TouchableOpacity, View} from 'react-native';
+import HeaderlessScreen from './screens/HeaderlessScreen';
 
 const App = () => {
-  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>;
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={({route}) => ({
+            headerBackVisible: false,
+            title: `상세정보: ${route.params.id}`,
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({children}) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
